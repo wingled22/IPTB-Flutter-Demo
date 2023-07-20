f4fb022b8bfd1e6033e9596545c1b148142b1b66
wingled22 <pelayowindel@gmail.com>
2023-07-20 11:25:46 +0800

    added textfield demo and button demo and setstate deom

---
+++ lib/lesson_archives/TextFieldDemo.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TFieldDemo extends StatefulWidget {
+  const TFieldDemo({super.key});
+
+  @override
+  State<TFieldDemo> createState() => _TFieldDemoState();
+}
+
+class _TFieldDemoState extends State<TFieldDemo> {
+  String tfieldValue = "No value yet";
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text(tfieldValue),
+             Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: textController,
+                decoration:  InputDecoration(
+                  prefix: const Icon(Icons.numbers),
+                  hintText: "Enter your crushyyyyy",
+                  suffix: IconButton(
+                      onPressed: (){
+                        textController.clear();
+                      }, 
+                      icon: const Icon(Icons.close)
+                    ),
+                  border:  const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue, 
+                        width: 10,
+                        style: BorderStyle.solid
+                      )
+                    )
+                ),
+              ),
+              ),
+            MaterialButton(
+              onPressed: (){
+
+                setState(() {
+                  tfieldValue = textController.text;
+                });
+                //textController.clear();
+               // tfieldValue = textController.text;
+              
+              },
+              color: Colors.blue,
+              child: const Text("Update upper text"),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
